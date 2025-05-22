f3a96e8bb7150f4e04a58bc97036237e62f2ab7c
2000123552 <sarthaks3@hexaware.com>
2025-05-22 15:10:01 +0530

    Updated code

--- utils/database.py
+++ utils/database.py
@@ -1,20 +1,4 @@
 from sqlalchemy import create_engine
-<<<<<<< Updated upstream
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./RenewableEnergyHackathon.db"
-
-# Create the engine
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-# Create a configured "Session" class
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class for all ORM models
-Base = declarative_base()
-=======
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
@@ -32,4 +16,3 @@ def get_db():
         yield db
     finally:
         db.close()
->>>>>>> Stashed changes
